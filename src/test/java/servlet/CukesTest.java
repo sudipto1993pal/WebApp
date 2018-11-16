bdc5ff4350496dfa8a7cac4f36e659827a08f85d
sudipto1993pal <45062749+sudipto1993pal@users.noreply.github.com>
2018-11-16 16:55:34 +0530

    Update CukesTest.java

--- src/test/java/servlet/CukesTest.java
+++ src/test/java/servlet/CukesTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({login_junit.class,createpage_junit.class})
+@Suite.SuiteClasses({login_junit.class,/*createpage_junit.class*/})
 public class CukesTest {
 	
 }
